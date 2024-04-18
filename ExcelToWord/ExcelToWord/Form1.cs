d62b6c68446a5af4cb1b56ebf92a90870dfbf3ee
Kbence9 <bence.kristian@gmail.com>
2024-04-18 10:27:58 +0200

    feat - add starter code and basic interface

---
+++ ExcelToWord/ExcelToWord/Form1.cs
@@ -0,0 +1,63 @@
+using ExcelToWord.Services;
+
+namespace ExcelToWord;
+
+public partial class Form1 : Form
+{
+    public string ExcelFile = "";
+    public string WordFile = "";
+
+    public Form1()
+    {
+        InitializeComponent();
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+        int size = -1;
+        OpenFileDialog openFileDialog1 = new();
+        DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+        if (result == DialogResult.OK) // Test result.
+        {
+            ExcelFile = openFileDialog1.FileName;
+            textBox1.Text = ExcelFile;
+            try
+            {
+                string text = File.ReadAllText(ExcelFile);
+                size = text.Length;
+            }
+            catch (IOException)
+            {
+            }
+        }
+        Console.WriteLine(size); // <-- Shows file size in debugging mode.
+        Console.WriteLine(result); // <-- For debugging use.
+    }
+
+    private void button2_Click(object sender, EventArgs e)
+    {
+        int size = -1;
+        OpenFileDialog openFileDialog1 = new();
+        DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+        if (result == DialogResult.OK) // Test result.
+        {
+            WordFile = openFileDialog1.FileName;
+            textBox2.Text = WordFile;
+            try
+            {
+                string text = File.ReadAllText(WordFile);
+                size = text.Length;
+            }
+            catch (IOException)
+            {
+            }
+        }
+        Console.WriteLine(size); // <-- Shows file size in debugging mode.
+        Console.WriteLine(result); // <-- For debugging use.
+    }
+
+    private void button3_Click(object sender, EventArgs e)
+    {
+        ConvertExcelToWord.ConvertFile(ExcelFile, WordFile);
+    }
+}
---
+++ ExcelToWord/ExcelToWord/Services/ConvertExcelToWord.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
+
+namespace ExcelToWord.Services
+{
+    static class ConvertExcelToWord
+    {
+        public static void ConvertFile(string excel, string word)
+        {
+
+            Microsoft.Office.Interop.Excel.Application _excelApp = new()
+            {
+                Visible = true
+            };
+
+            string fileName = excel;
+
+            Console.WriteLine(excel);
+
+            try
+            {
+                //open the workbook
+                Workbook workbook = _excelApp.Workbooks.Open(fileName,
+                    Type.Missing, Type.Missing, Type.Missing, Type.Missing,
+                    Type.Missing, Type.Missing, Type.Missing, Type.Missing,
+                    Type.Missing, Type.Missing, Type.Missing, Type.Missing,
+                    Type.Missing, Type.Missing);
+
+                //select the first sheet        
+                Worksheet worksheet = (Worksheet)workbook.Worksheets[1];
+
+                //find the used range in worksheet
+                Microsoft.Office.Interop.Excel.Range excelRange = worksheet.UsedRange;
+
+                //get an object array of all of the cells in the worksheet (their values)
+                object[,] valueArray = (object[,])excelRange.get_Value(
+                            XlRangeValueDataType.xlRangeValueDefault);
+
+                //access the cells
+                for (int row = 1; row <= worksheet.UsedRange.Rows.Count; ++row)
+                {
+                    for (int col = 1; col <= worksheet.UsedRange.Columns.Count; ++col)
+                    {
+                        //access each cell
+                        Debug.Print(valueArray[row, col].ToString());
+                    }
+                }
+
+                //clean up stuffs
+                workbook.Close(false, Type.Missing, Type.Missing);
+                Marshal.ReleaseComObject(workbook);
+
+                _excelApp.Quit();
+                Marshal.FinalReleaseComObject(_excelApp);
+            } 
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+        }
+    }
+}
