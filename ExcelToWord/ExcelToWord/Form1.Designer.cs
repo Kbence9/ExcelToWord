a9f2523f3be42e32df26d0119d43277bc4b5c476
Kbence9 <bence.kristian@gmail.com>
2024-05-17 10:57:37 +0200

    feat - add Form1.Designer.cs

--- ExcelToWord/ExcelToWord/Form1.Designer.cs
+++ ExcelToWord/ExcelToWord/Form1.Designer.cs
@@ -36,25 +36,28 @@ partial class Form1
         button3 = new Button();
         label1 = new Label();
         label2 = new Label();
+        button4 = new Button();
+        textBox3 = new TextBox();
+        label3 = new Label();
         SuspendLayout();
         // 
         // button1
         // 
-        button1.Location = new Point(485, 150);
+        button1.Location = new Point(485, 142);
         button1.Name = "button1";
-        button1.Size = new Size(141, 23);
+        button1.Size = new Size(141, 37);
         button1.TabIndex = 0;
-        button1.Text = "button1";
+        button1.Text = "Válassz Excel  fájl";
         button1.UseVisualStyleBackColor = true;
         button1.Click += button1_Click;
         // 
         // button2
         // 
-        button2.Location = new Point(485, 205);
+        button2.Location = new Point(485, 197);
         button2.Name = "button2";
-        button2.Size = new Size(141, 22);
+        button2.Size = new Size(141, 35);
         button2.TabIndex = 1;
-        button2.Text = "button2";
+        button2.Text = "Válassz Word fájlt";
         button2.UseVisualStyleBackColor = true;
         button2.Click += button2_Click;
         // 
@@ -74,11 +77,11 @@ partial class Form1
         // 
         // button3
         // 
-        button3.Location = new Point(360, 285);
+        button3.Location = new Point(314, 329);
         button3.Name = "button3";
-        button3.Size = new Size(148, 47);
+        button3.Size = new Size(238, 79);
         button3.TabIndex = 4;
-        button3.Text = "button3";
+        button3.Text = "Konvertálás";
         button3.UseVisualStyleBackColor = true;
         button3.Click += button3_Click;
         // 
@@ -87,24 +90,54 @@ partial class Form1
         label1.AutoSize = true;
         label1.Location = new Point(199, 132);
         label1.Name = "label1";
-        label1.Size = new Size(38, 15);
+        label1.Size = new Size(103, 15);
         label1.TabIndex = 5;
-        label1.Text = "label1";
+        label1.Text = "Excel fájl elérési út";
         // 
         // label2
         // 
         label2.AutoSize = true;
         label2.Location = new Point(199, 186);
         label2.Name = "label2";
-        label2.Size = new Size(38, 15);
+        label2.Size = new Size(105, 15);
         label2.TabIndex = 6;
-        label2.Text = "label2";
+        label2.Text = "Word fájl elérési út";
+        // 
+        // button4
+        // 
+        button4.Location = new Point(485, 251);
+        button4.Name = "button4";
+        button4.Size = new Size(141, 38);
+        button4.TabIndex = 7;
+        button4.Text = "Válassz cél mappát";
+        button4.UseVisualStyleBackColor = true;
+        button4.Click += button4_Click;
+        // 
+        // textBox3
+        // 
+        textBox3.Location = new Point(199, 260);
+        textBox3.Name = "textBox3";
+        textBox3.Size = new Size(258, 23);
+        textBox3.TabIndex = 8;
+        // 
+        // label3
+        // 
+        label3.AutoSize = true;
+        label3.Location = new Point(199, 242);
+        label3.Name = "label3";
+        label3.Size = new Size(124, 15);
+        label3.TabIndex = 9;
+        label3.Text = "Konvertált fájlok helye";
         // 
         // Form1
         // 
         AutoScaleDimensions = new SizeF(7F, 15F);
         AutoScaleMode = AutoScaleMode.Font;
+        BackColor = SystemColors.Window;
         ClientSize = new Size(845, 560);
+        Controls.Add(label3);
+        Controls.Add(textBox3);
+        Controls.Add(button4);
         Controls.Add(label2);
         Controls.Add(label1);
         Controls.Add(button3);
@@ -113,7 +146,7 @@ partial class Form1
         Controls.Add(button2);
         Controls.Add(button1);
         Name = "Form1";
-        Text = "Form1";
+        Text = "ExcelToWord";
         ResumeLayout(false);
         PerformLayout();
     }
@@ -127,4 +160,7 @@ partial class Form1
     private Button button3;
     private Label label1;
     private Label label2;
+    private Button button4;
+    private TextBox textBox3;
+    private Label label3;
 }
